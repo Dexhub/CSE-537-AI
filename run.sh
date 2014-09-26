875b2e8bbacd16580fa20cd36bda03d8b2b22fe9
Himanshu Shah <Himanshu1410@live.com>
2014-09-25 21:28:11 -0400

    Minor changes in the run script

--- run.sh
+++ run.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
 echo "#################################################"
-echo "Testing DFS"
+echo "                  Testing DFS                    "
 echo "#################################################"
 python pacman.py -l tinyMaze -p SearchAgent
 python pacman.py -l mediumMaze -p SearchAgent
 python pacman.py -l bigMaze -z .5 -p SearchAgent
 
 echo "#################################################"
-echo "Testing BFS"
+echo "			Testing BFS		       "
 echo "#################################################"
 python pacman.py -l tinyMaze -p SearchAgent -a fn=bfs
 python pacman.py -l mediumMaze -p SearchAgent -a fn=bfs
 python pacman.py -l bigMaze -p SearchAgent -a fn=bfs -z .5
 
 echo "#################################################"
-echo "Testing UCS"
+echo "			Testing UCS		       "
 echo "#################################################"
 python pacman.py -l tinyMaze -p SearchAgent -a fn=ucs
 python pacman.py -l mediumMaze -p SearchAgent -a fn=ucs
 python pacman.py -l bigMaze -p SearchAgent -a fn=ucs
 
 echo "#################################################"
-echo "Testing A* with heuristic"
+echo "		Testing A* with manhattan heuristic    "
 echo "#################################################"
 python pacman.py -l tinyMaze -z .5 -p SearchAgent -a fn=astar,heuristic=manhattanHeuristic 
 python pacman.py -l mediumMaze -z .5 -p SearchAgent -a fn=astar,heuristic=manhattanHeuristic 
 python pacman.py -l bigMaze -z .5 -p SearchAgent -a fn=astar,heuristic=manhattanHeuristic 
 
 echo "#################################################"
-echo "Testing A* with heuristic"
+echo "		Testing A* with Null heuristic         "
 echo "#################################################"
 python pacman.py -l tinyMaze -z .5 -p SearchAgent -a fn=astar 
 python pacman.py -l mediumMaze -z .5 -p SearchAgent -a fn=astar 
@@ -36,19 +36,19 @@ python pacman.py -l bigMaze -z .5 -p SearchAgent -a fn=astar
 
 
 echo "#################################################"
-echo "Corner Problem"
+echo "			Corner Problem		       "
 echo "#################################################"
 python pacman.py -l tinyCorners -p SearchAgent -a fn=bfs,prob=CornersProblem
 python pacman.py -l mediumCorners -p SearchAgent -a fn=bfs,prob=CornersProblem
 
 
 echo "#################################################"
-echo "Corner Problem with A*"
+echo "		   Corner Problem with A*	       "
 echo "#################################################"
 python pacman.py -l mediumCorners -p AStarCornersAgent -z 0.5
 
 echo "#################################################"
-echo "A* Food search agent"
+echo "		A* Food search agent		       "
 echo "#################################################"
 python pacman.py -l testSearch -p AStarFoodSearchAgent
 python pacman.py -l trickySearch -p AStarFoodSearchAgent
